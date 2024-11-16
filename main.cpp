4ffdccaa62aee47e4715554206a0d3ff74a5f014
Yubin Zhen <yubinzhen@ucr-secure-42-10-13-219-35.wnet.ucr.edu>
2024-11-16 11:42:09 -0800

    added displayMenu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 
 int main()
 {
+    int choice;
     cout << "Welcome to our quiz maker!" << endl;
+    cout << "(1) Add new flashcard" << endl;
+    cout << "(2) Study flashcards" << endl;
+    cout << "(3) Exit Program" << endl;
+    cin >> choice;
+    while (choice != 1 || choice != 2 || choice != 3) {
+        cout << "Invalid input. Please enter valid number.";
+        cin >> choice;
+    }
+
+    if (choice == 1) {
+        addFlashcards();
+    }
+
+    if (choice == 2) {
+        study();
+    }
+
+    if (choice == 3) {
+        return 0;
+    }
+
     return 0;
 }
