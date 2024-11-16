e16a332dc959ef2427a700e342b4492d9364a49b
wesley <wesleylwu@gmail.com>
2024-11-16 11:37:25 -0800

    added removeFlashcards and displayAllFlashcards

--- quiz.cpp
+++ quiz.cpp
@@ -22,3 +22,34 @@ void Quiz::addFlashcards() {
     Flashcards newFlashCard = Flashcards(question, answer);
     flashcard.push_back(newFlashCard);
 }
+
+void Quiz::removeFlashcards()
+{
+    int num;
+    vector<Flashcards> newFlashcards;
+    cout << "What number flashcard would you like to remove? ";
+
+    cin >> num;
+    num -= 1;
+
+    cout << endl;
+
+    for (int i = 0; i < flashcard.size(); ++i)
+    {
+        if (num != i)
+        {
+            newFlashcards.push_back(flashcard.at(i));
+        }
+    }
+
+    flashcard = newFlashcards;
+}
+
+void Quiz::displayAllFlashcards()
+{
+    for (int i = 0; i < flashcard.size(); ++i)
+    {
+        cout << i + 1 << ". ";
+        cout << "Q: " << flashcard.at(0).getQuestions() << " A: " << flashcard.at(0).getAnswers() << endl;
+    }
+}
