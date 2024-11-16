c0f46b27c3600efc04cf3bb5dcde982869f53fb5
wesley <wesleylwu@gmail.com>
2024-11-16 09:24:57 -0800

    switched

---
+++ quiz.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Quiz 
+{
+    private:
+        vector<string> questions;
+        vector<string> answers;
+
+    public:
+        
+};
